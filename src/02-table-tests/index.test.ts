66aa8494510cd5f922f7b4f75a73a0f4869cbbf3
Roman Pavlov <romlho13@gmail.com>
2024-10-19 22:37:01 +0400

    feat: implement table tests

--- src/02-table-tests/index.test.ts
+++ src/02-table-tests/index.test.ts
@@ -1,17 +1,34 @@
-// Uncomment the code below and write your tests
-/* import {  simpleCalculator, Action } from './index';
+import { simpleCalculator, Action } from './index';
 
 const testCases = [
-    { a: 1, b: 2, action: Action.Add, expected: 3 },
-    { a: 2, b: 2, action: Action.Add, expected: 4 },
-    { a: 3, b: 2, action: Action.Add, expected: 5 },
-    // continue cases for other actions    
-]; */
+  { a: 1, b: 2, action: Action.Add, expected: 3 },
+  { a: 2, b: 2, action: Action.Add, expected: 4 },
+  { a: 3, b: 2, action: Action.Add, expected: 5 },
+
+  { a: 3, b: 2, action: Action.Subtract, expected: 1 },
+  { a: 10, b: 2, action: Action.Subtract, expected: 8 },
+  { a: 20, b: 2, action: Action.Subtract, expected: 18 },
+
+  { a: 10, b: 2, action: Action.Multiply, expected: 20 },
+  { a: 10, b: 3, action: Action.Multiply, expected: 30 },
+  { a: 20, b: 2, action: Action.Multiply, expected: 40 },
+
+  { a: 10, b: 2, action: Action.Divide, expected: 5 },
+  { a: 15, b: 3, action: Action.Divide, expected: 5 },
+  { a: 20, b: 2, action: Action.Divide, expected: 10 },
+
+  { a: 10, b: 2, action: Action.Exponentiate, expected: 100 },
+  { a: 3, b: 3, action: Action.Exponentiate, expected: 27 },
+  { a: 20, b: 2, action: Action.Exponentiate, expected: 400 },
+
+  { a: 1, b: 2, action: null, expected: null },
+
+  { a: '1', b: 2, action: Action.Add, expected: null },
+  { a: 1, b: '2', action: Action.Add, expected: null },
+];
 
 describe('simpleCalculator', () => {
-  // This test case is just to run this test suite, remove it when you write your own tests
-  test('should blah-blah', () => {
-    expect(true).toBe(true);
+  test.each(testCases)('%p', ({ expected, ...payload }) => {
+    expect(simpleCalculator(payload)).toBe(expected);
   });
-  // Consider to use Jest table tests API to test all cases above
 });
